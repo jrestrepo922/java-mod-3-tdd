b467ef6b226b5b1837eb011f0bd827fa4f218543
Juan Martin Restrepo <tinto922@gmail.com>
2022-07-12 14:02:10 -0400

    completed

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -8,4 +8,16 @@ public class FizzBuzz {
       if (str.endsWith("b")) return "Buzz";
       return str;
   }
+
+  public String[] fizzBuzzArray(String[] arr){
+      if(arr == null || arr.length == 0 ){
+          System.out.println("arrays can not be null or empty");
+          return null;
+      }
+      for(int i = 0; i < arr.length; i++){
+          String newValue = this.fizzBuzzString(arr[i]);
+          arr[i] = newValue;
+      }
+      return arr;
+  }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -2,6 +2,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
@@ -38,6 +42,32 @@ class FizzBuzzTest {
         String actualValue = fizzBuzz.fizzBuzzString(null);
         String expectedValue = null;
         assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void fizzBuzzArrayFilledCorrectlyTest() {
+        String[] sampleArr = {"fun", "stab", "funb", "no"};
+        String[] actualArr = fizzBuzz.fizzBuzzArray(sampleArr);
+        String[] expectedArr = {"Fizz", "Buzz", "FizzBuzz", "no"};
+        for(int i = 0; i < sampleArr.length; i++){
+            assertEquals(expectedArr[i], actualArr[i]);
+        }
+        List<String> arr = new ArrayList<String>(Arrays.asList(actualArr));
+        System.out.println(arr);
+    }
+
+    @Test
+    void fizzBuzzArrayNullTest() {
+        String[] sampleArr = null;
+        String[] actualArr = fizzBuzz.fizzBuzzArray(sampleArr);
+        assertEquals(null, actualArr);
+    }
+
+    @Test
+    void fizzBuzzArrayEmptyTest() {
+        String[] sampleArr = {};
+        String[] actualArr = fizzBuzz.fizzBuzzArray(sampleArr);
+        assertEquals(null, actualArr);
     }
 
 }
